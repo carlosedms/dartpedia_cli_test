0f03a2814ced192524dc836701e4a38df0220c24
carlosedms <cems2002@outlook.com>
2026-06-20 21:08:47 -0300

    functions placed in main binaries file + mild funcionalities added (help/usage message and search foundation)

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,13 +1,38 @@
-import 'package:cli/cli.dart';
+import 'dart:io';
 
 void main(List<String> arguments) {
   const String version = '1.0.0';
 
-  if (arguments.isEmpty || arguments[0] == "-h" || arguments[0] == "--help") {
-    printHelp();
-  } else {
-    for(int i = 0; i < arguments.length; i++){
-      print(i);
+  if(arguments.isNotEmpty){
+    for(int i=0; i<arguments.length; i++){
+      if(arguments[i] == "-h" || arguments[i] == "--help"){
+        printHelp();
+      }else if(arguments[i] == "-v" || arguments[i] == "--version"){
+        print("Dartpedia CLI version $version");
+      }else if(arguments[i] == "search"){
+        print("Searching.");
+      }else{
+        printHelp();
+      }
+
     }
+
+  }else{
+    printHelp();
   }
+}
+
+
+void printHelp() {
+  print("-- Dartpedia CLI usage --");
+  print("\nUSAGE:");
+  print("dart cli.dart [flags] {parameter}");
+  
+  print("\nFLAGS:");
+  print("'-h' or '--help': prints to the terminal this usage guide.");
+  print("'-v' or '--version': prints to the terminal the current version of this program");
+
+  print("\nPARAMETERS:");
+
+  exit(0);
 }
--- lib/cli.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-void printHelp() {
-  print("Dartpedia CLI usage:\n");
-
-  exit(0);
-}
